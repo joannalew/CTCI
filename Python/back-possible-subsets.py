da63c3a858c6ae8356ade8347ed7565f2f1b3621
Joanna <lewj@oregonstate.edu>
2019-01-24 10:19:10 -0800

    rename

--- Python/back-possible-subsets.py
+++ Python/back-possible-subsets.py
@@ -1,7 +1,7 @@
 # CTCI 8.3
 # Write a method that returns all subsets of a set
 
-# Using bitwise operators
+# Iterative: using bitwise operators
 # Total number of subsets = 2^n, where n is length of set
 # Notice that binary representation of (0...2^n) correlates to elements in subset
 
@@ -21,12 +21,39 @@ def findSubsets(s):
 	return res
 
 
+# Backtracking
+# Have two choices for each element: include or don't
+def findSubsetsBack(s):
+	res = []
+	sub = []
+
+	res.append(set(sub))			# add the null set
+	subsetsUtil(s, res, sub, 0)
+
+	return res
+
+def subsetsUtil(s, res, sub, index):
+	for i, el in enumerate(s):				# for i in range(index, len(s))
+		if i >= index: 
+			sub.append(el)					# sub.append(s[i])
+			res.append(set(sub))
+
+			subsetsUtil(s, res, sub, i+1)
+
+			sub.pop()
+	
+	return
+
 def main():
 	s = {1, 2, 3, 4, 5, 6, 7}
 	subsets = sorted(findSubsets(s), key=len)
+	subsetsBack = sorted(findSubsetsBack(s), key=len)
 
 	for sub in subsets:
 		print (sub)
+
+	print (len(subsets) == len(subsetsBack))
+	
 
 
 if __name__ == "__main__": 
