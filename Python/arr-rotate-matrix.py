74570fb9d2ca9db051d41b7288eba45d6f94b898
Joanna <lewj@oregonstate.edu>
2019-01-26 23:05:00 -0800

    add vscode settings

--- Python/arr-rotate-matrix.py
+++ Python/arr-rotate-matrix.py
@@ -1,4 +1,4 @@
-# CTCI 1.7 
+# CTCI 1.7
 # Given a N x N matrix, rotate the matrix by 90 degrees
 
 # [ 1, 2, 3 ]           [ 3, 6, 9 ]
@@ -9,26 +9,26 @@
 # [ 10 11 12 ]    =>    [ 01 11 21 ]
 # [ 20 21 22 ]          [ 00 10 20 ]
 
-# Method: create another array, and 
+# Method: create another array, and
 # store the element at the corresponding index
 def rotate_matrix(mat):
-	rows = len(mat)
-	cols = len(mat[0])
-	res = [ [0 for j in range(cols)] for x in range(rows) ]
+    rows = len(mat)
+    cols = len(mat[0])
+    res = [[0 for j in range(cols)] for x in range(rows)]
 
-	for i in range(rows):
-		for j in range(cols):
-			res[i][j] = mat[j][rows - i - 1] 
-    
-	return res
+    for i in range(rows):
+        for j in range(cols):
+            res[i][j] = mat[j][rows - i - 1]
+
+    return res
 
 
 def print_matrix(mat):
-	for row in mat:
-		print(row)
+    for row in mat:
+        print(row)
 
 def main():
-	print_matrix(rotate_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    print_matrix(rotate_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
 
-if __name__ == "__main__": 
-	main()
+if __name__ == "__main__":
+    main()
