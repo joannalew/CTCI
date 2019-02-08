18bf7ba35885af7eaf9ae53b3000b36f4a341ab9
Joanna <lewj@oregonstate.edu>
2019-02-07 23:27:53 -0800

    add leetcode house robber problem

---
+++ Python/dp-house-robber.py
@@ -0,0 +1,23 @@
+# LeetCode 198
+# You're a house robber, robbing houses on a street.
+# Each house has a certain amount of money stashed.
+# You can't rob houses that are adjacent to each other.
+# Determine the max amount of money you can rob tonight.
+
+# Example 1: robber([1, 2, 3, 1])
+#   Output: 4
+#   Rob house #1 ($1), and house #3 ($3) --> $4
+
+# Example 2: robber([2, 7, 9, 3, 1])
+#   Output: 12
+#   Rob house #1 ($2), house #3 ($9), house #5 ($1) --> $12
+
+def robber(nums):
+
+
+def main():
+    print(robber([1, 2, 3, 1])) # => 4
+    print(robber([2, 7, 9, 3, 1])) # => 12
+
+if __name__ == "__main__":
+    main()
