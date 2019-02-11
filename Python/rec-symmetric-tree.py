4887562ba5b28754493836ade1fcb5b9d97fa154
Joanna <lewj@oregonstate.edu>
2019-02-11 11:03:55 -0800

    recursion symmetric tree description

--- Python/rec-symmetric-tree.py
+++ Python/rec-symmetric-tree.py
@@ -1,3 +1,10 @@
+# LeetCode 101: Symmetric Tree
+# Given a binary tree, check whether it is a mirror of itself 
+# (ie, symmetric around its center).
+
+# For example, this binary tree [1, 2, 2, 3, 4, 4, 3] is symmetric
+# But the tree [1, 2, 2, null, 3, null, 3] is not
+
 class TreeNode:
 	def __init__(self, x):
 		self.val = x
