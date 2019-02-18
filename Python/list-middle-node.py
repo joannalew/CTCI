ecd865a50d7f6f747df1f20f6d8a61ce64a73972
Joanna <lewj@oregonstate.edu>
2019-02-17 23:19:10 -0800

    add description to middle linked list

--- Python/list-middle-node.py
+++ Python/list-middle-node.py
@@ -1,3 +1,17 @@
+# LeetCode 876: Middle of the Linked List
+# Given a non-empty, singly linked list with head node head, 
+#   return a middle node of linked list.
+
+# Example: Input: [1,2,3,4,5]
+# Output: Node 3 from this list (Serialization: [3,4,5])
+# The returned node has value 3.  
+#   (The judge's serialization of this node is [3,4,5]).
+# Note that we returned a ListNode object ans, such that:
+#   ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 
+#   and ans.next.next.next = NULL.
+
+If there are two middle nodes, return the second middle node.
+
 class ListNode:
     def __init__(self, x):
         self.val = x
