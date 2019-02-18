c759c1ae56f96cd450e1f1424e1f37d9784beffa
Joanna <lewj@oregonstate.edu>
2019-02-17 23:57:12 -0800

    add description to buy sell stock

--- Python/dp-buy-sell-stock.py
+++ Python/dp-buy-sell-stock.py
@@ -1,3 +1,17 @@
+# LeetCode 121: Best Time to Buy and Sell Stock
+# Say you have an array for which the ith element 
+#   is the price of a given stock on day i.
+# If you were only permitted to complete at most one transaction
+#   (i.e., buy one and sell one share of the stock), 
+#   design an algorithm to find the maximum profit.
+# Note that you cannot sell a stock before you buy one.
+
+# Example: Input: [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), 
+#   profit = 6-1 = 5.
+
+
 def maxProfit(prices):
     numPrices = len(prices)
     if numPrices < 2:
